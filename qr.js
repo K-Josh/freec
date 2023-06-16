930ddbcf1d77971da62e4e648a7c77ae489acb90
K-Josh <97101966+K-Josh@users.noreply.github.com>
2023-06-16 19:33:39 +0200

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import inquirer from "inquirer";
+import qr  from "qr-image";
+
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+      message: "enter your URL",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+    
+    fs.writeFile('URL.txt', url, (err) => {
+      if (err) throw err;
+      console.log('the file has been saved');
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
